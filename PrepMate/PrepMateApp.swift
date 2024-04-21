d3cd2fef3a4897a916b3bc10f5923575d7b7bc17
AndresMadeDev <andres.made.dev@gmail.com>
2024-04-21 17:37:03 -0400

    Initial Commit

---
+++ PrepMate/PrepMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepMateApp.swift
+//  PrepMate
+//
+//  Created by Andres Made on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PrepMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
