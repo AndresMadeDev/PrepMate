b1aa33c238862b3accb7ae69afef7d73392e42e6
AndresMadeDev <andres.made.dev@gmail.com>
2024-04-21 23:15:05 -0400

    Version 2

---
+++ PrepMate/TodoCellView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoCellView.swift
+//  PrepMate
+//
+//  Created by Andres Made on 4/21/24.
+//
+
+import SwiftUI
+
+struct TodoCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoCellView()
+}
